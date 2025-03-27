3682104b164d3a6c44ce722102c05319e0cfe9a3
usamaahma <usamaahmad087@gmail.com>
2025-03-27 16:18:31 +0500

    withdraw issue

--- src/models/withdraw.model.js
+++ src/models/withdraw.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const withdrawSchema = new mongoose.Schema(
   {
-    user: {
+    userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
       required: true,
