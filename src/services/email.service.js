685e5da59bd35671f472e92829d926b8dd2647cc
usamaahma <usamaahmad087@gmail.com>
2025-04-14 16:53:37 +0500

    usama

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -32,7 +32,7 @@ const sendEmail = async (to, subject, text) => {
 const sendResetPasswordEmail = async (to, token) => {
   const subject = 'Reset password';
   // replace this url with the link to the reset password page of your front-end app
-  const resetPasswordUrl = `https://aljadeedcapitals.vercel.app/forget-password?token=${token}`;
+  const resetPasswordUrl = `https://aljadeedcapitals.vercel.app/res-password?token=${token}`;
   const text = `Dear user,
 To reset your password, click on this link: ${resetPasswordUrl}
 If you did not request any password resets, then ignore this email.`;
